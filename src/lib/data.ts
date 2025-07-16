312ecb4c5b423b2bdf18035d3d472d7a3139bd77
p1927316 <ghiles-mohamed.boussahla@etu.univ-lyon1.fr>
2025-07-16 22:38:07 +0200

    added Education and updated projects

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -54,15 +54,24 @@ export const workExperience = [
 
 export const education = [
   {
-    institution: "Sri Krishna College of Engineering and Technology",
-    location: "Coimbatore, India",
-    degree: "B Tech Information Technology",
-    period: "Jun 2021 - Jun 2025",
+    institution: "IA School",
+    location: "Boulogne-Billancourt, France",
+    degree: "Master Intelligence Artificielle et Data Science",
+    period: "Sép 2022 - Mai 2024",
     achievements: [
-      "President of the Department of Information Technology",
-      "G20 student delegate",
-      "Organized multiple technical workshops and events",
-      "Represented my college in multiple national and international hackathons and won.",
+      "Spécialisé en NLP, Deep Learning et gestion de projets IA.",
+      "Modélisation, analyse statistique, scraping et API.",
+    ],
+  },
+  {
+    institution: "Université Claude Bernard Lyon 1",
+    location: "Lyon, France",
+    degree: "Licence Informatique",
+    period: "Sép 2018 - Mai 2022",
+    achievements: [
+      "Formation en informatique à l’Université Claude Bernard Lyon 1, avec une spécialisation progressive de la programmation (C++, Python, PHP) et des bases de données SQL.",
+      "Réalisation de projets pratiques tels qu’un site web mini-Pinterest, un jeu Tetris en C++, et un explorateur interactif de données COVID via Jupyter Notebook.",
+      "Acquisition de compétences solides en algorithmique, programmation orientée objet, conception UML, et analyse de données, accompagnée d’auto-formation sur des bibliothèques Python pour le traitement et la visualisation des données.",
     ],
   },
 ];
@@ -127,77 +136,25 @@ export const skills = {
 
 export const projects = [
   {
-    title: "Net Zero Carbon Emissions",
-    github: "https://github.com/rishikesh2003/Prodigi",
+    title: "Détection de fake news",
+    github: "https://github.com/GhilesBo/detection_fake_news_M2",
     description: [
-      "WiFi-RTT: Developed indoor occupancy tracking for energy optimization.",
-      "IoT Solutions: Implemented smart monitoring for energy efficiency and food waste reduction.",
-      "Real-Time Tracking: Designed systems to monitor carbon emissions and optimize resources.",
-      "Data-Driven Insights: Analyzed user patterns for adaptive energy and food management.",
-      "Reward Integration: Built QR-based green points system to incentivize eco-friendly actions.",
+      "Les fake news, ou fausses informations, sont des contenus délibérément trompeurs ou mensongers qui sont conçus pour manipuler ou tromper le public. Face à ce défi, il est essentiel de développer des outils et des techniques pour identifier et contrer la désinformation.",
+      "Utilisation de techniques de deep learning pour la détection de fake news.",
+      "Entrainement de modèles sur des ensembles de données étiquetées pour classer les nouvelles comme vraies ou fausses.",
     ],
   },
-  {
-    title: "Mental Aarog",
-    github: "https://github.com/rishikesh2003/mental-aarog",
-    description: [
-      "A holistic mental health app leveraging AI and blockchain for early detection and personalized solutions for depression.",
-      "Features include social media analysis (ML-based sentiment tracking), PHQ-9 assessments, smartwatch integration for sleep and activity data, guided meditation, and smart suggestions for food, travel, music, and movies.",
-      "Developed a rewards system using MAG crypto token on Ethereum, enabling user engagement through in-app incentives.",
-      "Secure storage implemented via web3.storage, IPFS, and FileCoin.",
-      "Built with React, Supabase, Node.js, Flask, and Solidity, showcasing seamless integration of health tech and blockchain.",
-    ],
-  },
+  //{
+    //title: "Mental Aarog",
+    //github: "https://github.com/rishikesh2003/mental-aarog",
+    //description: [
+    //  "A holistic mental health app leveraging AI and blockchain for early detection and personalized solutions for depression.",
+    // "Features include social media analysis (ML-based sentiment tracking), PHQ-9 assessments, smartwatch integration for sleep and activity data, guided meditation, and smart suggestions for food, travel, music, and movies.",
+    // "Developed a rewards system using MAG crypto token on Ethereum, enabling user engagement through in-app incentives.",
+    // "Secure storage implemented via web3.storage, IPFS, and FileCoin.",
+    // "Built with React, Supabase, Node.js, Flask, and Solidity, showcasing seamless integration of health tech and blockchain.",
+    //],
+  //},
 ];
 
-export const awards = [
-  {
-    name: "IEEE YESIST12 Hackathon",
-    issuer: "IEEE",
-    date: "Sep 2022",
-    type: "International",
-    position: "Second Place",
-  },
-  {
-    name: "Prodigi Cognizant Hackathon",
-    issuer: "Cognizant",
-    date: "Feb 2023",
-    type: "National",
-    position: "Second Runner-up",
-  },
-  {
-    name: "Cisco Thingqbator Hackathon",
-    issuer: "Cisco",
-    date: "Jan 2023",
-    type: "National",
-    position: "First Runner-up",
-  },
-  {
-    name: "Innovators Day",
-    issuer: "Sri Manakula Vinayagar Engineering College, Pondicherry",
-    date: "Sep 2022",
-    type: "National",
-    position: "First Prize",
-  },
-  {
-    name: "KG Hackfest'22",
-    issuer: "KGiSL Institute of Technology, Coimbatore",
-    date: "Sep 2022",
-    type: "National",
-    position: "Second Prize",
-  },
-  {
-    name: "Innohacks'22",
-    issuer: "Innogeeks, KIET Group of Institutions, New Delhi",
-    date: "May 2022",
-    type: "National",
-    position: "Second Runner-up",
-  },
-  {
-    name: "Hack @ SKCET",
-    issuer: "Hackclub SKCET, SKCET, Coimbatore",
-    date: "Feb 2022",
-    type: "National",
-    position: "Most Impactful Hack",
-  },
-];
+
